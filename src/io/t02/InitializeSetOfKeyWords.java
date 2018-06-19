06b586b7459f05bd413596177c43f3dac6295f88
Aydar <dusmukhambetov@mail.ru>
2018-06-19 13:49:05 +0300

    Result in output

---
+++ src/io/t02/InitializeSetOfKeyWords.java
@@ -0,0 +1,24 @@
+package io.t02;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class InitializeSetOfKeyWords {
+    static HashSet<String> initialize() throws IOException {
+        FileReader fr = new FileReader("./resources/KeyWords");
+        BufferedReader br = new BufferedReader(fr);
+
+        HashSet<String> keyWords = new HashSet<>();
+
+        while (br.ready()) {
+            String s = br.readLine();
+            keyWords.add(s.trim());
+        }
+
+        br.close();
+        return keyWords;
+    }
+}
---
+++ src/io/t02/Main.java
@@ -0,0 +1,46 @@
+package io.t02;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main {
+    static HashMap<String, Integer> result = new HashMap<>();
+
+    static void addInHashmap(String s) {
+        if (result.containsKey(s))
+            result.put(s, result.get(s) + 1);
+        else
+            result.put(s, 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("./resources/Notepad.java");
+        BufferedReader br = new BufferedReader(fr);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (br.ready()) {
+            int b = br.read();
+            if (b >= 97 && b <= 122) {
+                sb.append((char)b);
+            }
+            else if ((!sb.toString().equals("")) && InitializeSetOfKeyWords.initialize().contains(sb.toString())) {
+                addInHashmap(sb.toString());
+                sb = sb.delete(0, sb.length());
+            }
+            else
+                sb = sb.delete(0, sb.length());
+        }
+
+        br.close();
+
+        FileWriter fw = new FileWriter("./output/io/t02/result");
+
+        for (String s : result.keySet()) {
+            String out = s + " : " + result.get(s) + "\n";
+            fw.write(out);
+        }
+        fw.close();
+    }
+}
