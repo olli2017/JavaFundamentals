712dbe7722226e0d69ed701cfb5ddee8406e2e24
Aydar <dusmukhambetov@mail.ru>
2018-06-19 13:35:31 +0300

    Result in output

---
+++ src/io/t01/InitializeSetOfKeyWords.java
@@ -0,0 +1,27 @@
+package io.t01;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class InitializeSetOfKeyWords {
+
+    static HashSet<String> initialize() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("./resources/KeyWords");
+
+        HashSet<String> keyWords = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        while (fileInputStream.available() > 0) {
+            int b = fileInputStream.read();
+            if (b >= 97 && b <= 122) {
+                sb.append((char) b);
+            } else if (!sb.toString().equals("")) {
+                keyWords.add(sb.toString());
+                sb = sb.delete(0, sb.length());
+            }
+        }
+        fileInputStream.close();
+        return keyWords;
+    }
+}
---
+++ src/io/t01/Main.java
@@ -0,0 +1,47 @@
+package io.t01;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Main {
+    static HashMap<String, Integer> result = new HashMap<>();
+
+    static void addInHashmap(String s) {
+        if (result.containsKey(s))
+            result.put(s, result.get(s) + 1);
+        else
+            result.put(s, 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fileInputStream = new FileInputStream("./resources/Notepad.java");
+
+        StringBuilder sb = new StringBuilder();
+
+        while (fileInputStream.available() > 0) {
+            int b = fileInputStream.read();
+            if (b >= 97 && b <= 122) {
+                sb.append((char)b);
+            }
+            else if ((!sb.toString().equals("")) && InitializeSetOfKeyWords.initialize().contains(sb.toString())) {
+                addInHashmap(sb.toString());
+                sb = sb.delete(0, sb.length());
+            }
+            else
+                sb = sb.delete(0, sb.length());
+        }
+
+        fileInputStream.close();
+
+        FileOutputStream outputStream = new FileOutputStream("./output/io/t01/result");
+
+        for (String s : result.keySet()) {
+            String out = s + " : " + result.get(s) + "\n";
+            outputStream.write(out.getBytes());
+        }
+        outputStream.close();
+    }
+}
